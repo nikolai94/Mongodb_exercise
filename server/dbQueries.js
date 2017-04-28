bc15888183b539b78beb600d7162538f03ff099a
Nikolai94 <nikolai@cybermanager.dk>
2017-04-28 09:05:09 +0200

    Exercise - 1,3,4,5 - Done - To do exercise 2

--- server/dbQueries.js
+++ server/dbQueries.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var tweets = mongoose.model('Tweets');
 
 //countTwitterUsers();
-//How many Twitter users are in our database?
+//How many Twitter users are in our database? - DONE
 function countTwitterUsers() {
     tweets.aggregate(
         {$group : {_id : "$user"} },
@@ -14,26 +14,80 @@ function countTwitterUsers() {
         });
 }
 
-topTenTwitterUsersLink();
+
+//MAKE THIS
+//topTenTwitterUsersLink();
 //Which Twitter users link the most to other Twitter users? (Provide the top ten.)
 function topTenTwitterUsersLink() {
     tweets.aggregate(
-        [{"$match":{"text": {"$regex": "@"} }},
-            //{ "$group": {"_id":"$user", "count":{"$sum":1} }},
-            { "$group": {"_id":"$user", "tweets":{ "$sum":1 } }},
+            [{"$match":{"text": {"$regex": "@\\w+"} }},
+            { "$unwind" : "$user" },
             { "$sort": {"tweets": -1}},
             { "$limit" : 10 }],
         function(err,data){
             if(err){console.log(err); return;}
-            //console.log("There is '"+data[0].count +"' twitter users unique");
+            console.log(data);
+        });
+}
+
+//topFiveMostMentionedUsers();
+//Who is are the most mentioned Twitter users? - DONE
+function topFiveMostMentionedUsers() {
+    tweets.aggregate(
+        [{"$match":{"text": {"$regex" : "@\w+"} }},
+        {"$group": {"_id": {"user":"$user", "id":"$id"} }},
+        {"$group": {"_id":"$_id.user", "count":{"$sum":1} }},
+        { "$sort": {"count": -1}},
+        { "$limit" : 5 }],
+        function(err,data){
+            if(err){console.log(err); return;}
             console.log(data);
         });
 }
 
 
-/*function insertTweet() {
-    tweets.create({polarity:1, id:1, date:"test",query:"test",user:"test",text:"test"},function(err, data){
-        if(err) {console.log(err); return;}
-        console.log(data);
-    });
-}*/
+//mostActiveTwitterUsers();
+//Who are the most active Twitter users - DONE
+function mostActiveTwitterUsers() {
+    tweets.aggregate(
+            [{ "$group": {"_id":"$user", "tweets":{ "$sum":1 } }},
+            { "$sort": {"tweets": -1}},
+            { "$limit" : 10 }],
+        function(err,data){
+            if(err){console.log(err); return;}
+            console.log(data);
+        });
+
+}
+
+mostGrumpy();
+//Make this
+//Most grumpy limit 5
+function mostGrumpy(){
+    tweets.aggregate(
+        [{"$match":{"polarity": 0 }},
+            { "$group" : {"_id":"$user", "mostGrumpyCount":{"$sum":1}} },
+            { "$sort" : {"mostGrumpyCount": -1}},
+            { "$limit" : 5 }],
+        function(err,data){
+            if(err){console.log(err); return;}
+            console.log(data);
+        });
+}
+
+
+//mostHappy();
+//Most happy limit 5 - DONE
+function mostHappy(){
+    tweets.aggregate(
+        [{"$match":{"polarity": 4 }},
+            { "$group" : {"_id":"$user", "mostHappyCount":{"$sum":1}} },
+            { "$sort" : {"mostHappyCount": -1}},
+            { "$limit" : 5 }],
+        function(err,data){
+            if(err){console.log(err); return;}
+            console.log(data);
+        });
+}
+
+
